c9f77155a584b1a8ff4ab1a6e675679f0ad6ba3a
Yuriy <yuriy.konorev@normet.com>
2022-08-05 18:28:21 +0600

    add str

--- C001/Program.cs
+++ C001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2022Hello, World!");
+﻿// поиск максимального числа
+Console.WriteLine("Поиск максимального");
+Console.Write("первое число ");int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("второе число ");int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("третье число ");int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("четвёртое число ");int d = Convert.ToInt32(Console.ReadLine());
+Console.Write("пятое число ");int e = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if ( a > max ) max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+if ( d > max ) max = d;
+if ( e > max ) max = e;
+Console.WriteLine($"Максимальное число = { max }");
